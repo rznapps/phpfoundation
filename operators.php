d01dcfbc00bc0cca847ca2e0c1bf978b8f66e168
Razin Anik <rznanik@gmail.com>
2014-07-09 12:12:48 +0600

    Changes to be made

--- operators.php
+++ operators.php
@@ -6,10 +6,25 @@ $z = 100/(2*5);
 $a =0/5;
 $b =0%5;
 
+
+
 echo $x."<br />";
 echo $y."<br />";
 echo $z."<br />";
 echo $a."<br />";
 echo $b."<br />";
+
+$a=2;
+echo $a++."<br />";
+echo ++$a."<br />";
+echo $a."<br />";
+
+echo $a.PHP_EOL;
+echo ++$a."<br/>"; 
+
+$a=4;
+$b=2;
+
+echo   ++$a*($a++ + --$b);
 
 ?>
