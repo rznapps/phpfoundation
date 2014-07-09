ef8c827cbdac684bf3ef72c270cb1502f9134a52
Razin Anik <rznanik@gmail.com>
2014-07-09 13:19:28 +0600

    Array is working fine

--- constants.php
+++ constants.php
@@ -1,8 +1,22 @@
 <?php
 define('PIE', 3.14);
 echo PIE ; 
+// Array 
+
+$cars  = array('toyota','mercedes','lambargini');
+echo "<br/>.........ec...............";
+echo gettype($cars);
+echo "<br/>.........";
+//echo $; 
+print_r($cars);  
+echo "<br/>.........";
+echo $cars[1];
+echo "<br/>.........";
 
 
+$fruits = array ('a'=>'apple','b'=> 'banana'); 
 
-
+print_r($fruits);  
+echo "<br/>.........";
+echo $fruits['b'];
 ?>
