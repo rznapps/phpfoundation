61e3eb77538a24461709175c3dafc9350bfc5d22
Razin Anik <rznanik@gmail.com>
2014-07-10 12:44:48 +0600

    Accessing a method

--- oop.php
+++ oop.php
@@ -4,12 +4,19 @@ class User
 {
 var $name="FTFL";
 var $email="";
+
+function getName(){
+return "FTFL";
+
+}
 }
  
 // instantiation of a User
  $user= new User(); 
  var_dump($user); 
 echo $user->name;
+
+echo $user->getName(); 
 
 ?>
 
