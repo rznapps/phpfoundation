6eb719afd634ce6a166987f0e24957cad19cabaf
Razin Anik <rznanik@gmail.com>
2014-07-10 11:47:07 +0600

    Forst change

--- functiontest.php
+++ functiontest.php
@@ -3,46 +3,25 @@
 <?php
 
 function showinfo($gen){
+
+
 $fields = array
   (
-  "genres"-> array("comedy","tragedy","action","romance"),
-  "film_titles->"array("Big","star wars","titanic","french kiss"),
-  "stars"->array("Bill murrary","mark hammit","Leo nard de carprio","kate blacnchet"),
-  
+  "genres" => array("comedy","tragedy","action","romance"),
+  "film_titles" => array("Big","star wars","titanic","french kiss"),
+  "stars"=> array("Bill murrary","mark hammit","Leo nard de carprio","kate blacnchet"),
   );
 
-foreach ($testvar as $gen => $value) {
-	# code...
-}
+$result = $gen;
+foreach ($fields as key=>$value) {
+    if ($value[0] == $number) { 
+        $result = $value; 
+        break;
+    }
 
-
-
-
-
-
-
-
-
-
-
-}
-
-
-echo showinfo("comedy");
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    }
+    
+showinfo("comedy");
 
 ?>
+
