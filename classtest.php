731ea4b4a2c261478462c05c369076b8e62dcbc3
Razin Anik <rznanik@gmail.com>
2014-07-13 10:05:54 +0600

    Form submission using php

---
+++ classtest.php
@@ -0,0 +1,21 @@
+<?php
+
+class Employee {
+public $id;
+public $name;
+public $salary; 
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+?>
--- out.php
+++ out.php
@@ -2,7 +2,7 @@
 
 <body>
 
-<form action="out.php",method="POST">
+<form action="out.php",method="GET">
 Name: <input type="text" name='Name'><br>
 ID: <input type="text" name='Id'><br>
 Salary: <input type="text" name='salary'><br>
@@ -20,16 +20,23 @@ Salary: <input type="text" name='salary'><br>
 
 
 <?php
+require 'classtest.php'; 
 if (isset($_GET['Submit']))
 {
-$id= $_GET['Id'];
+$employeeobject = new Employee();
+$employeeobject->id=$_GET['Id'];
+$employeeobject->Name=$_GET['Name'];
+
+$employeeobject->Salary=$_GET['salary'];
+
+/*$id= $_GET[I'd'];
 $Name= $_GET['Name'];
-$Salary= $_GET['salary'];
+$Salary= $_GET['salary'];*/
 
 
-echo $id."<br/>"; 
-echo $Name."<br/>";
-echo $Salary."<br/>" ; 
+echo $employeeobject->id."<br/>"; 
+echo $employeeobject->Name."<br/>";
+echo $employeeobject->Salary."<br/>" ; 
 }
 
 
